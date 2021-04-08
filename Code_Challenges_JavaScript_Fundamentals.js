63ce71a304ca73e73c71a9a3fecd41923941862d
Jasmine Neal <75139330+jasbneal@users.noreply.github.com>
2021-04-07 20:43:53 -0400

    Add files via upload

--- Code_Challenges_JavaScript_Fundamentals.js
+++ Code_Challenges_JavaScript_Fundamentals.js
@@ -101,3 +101,57 @@ const finalGrade = (grade1, grade2, grade3) => {
     }
 
 console.log(finalGrade(90, 90, 95)) // Prints A
+
+/* 7 April 2021
+Write a function reportingForDuty(), that has 2 string parameters, rank and lastName,
+and returns a string in the following format: 'rank lastName reporting for duty!' */
+
+function reportingForDuty(rank, lastName) {
+    return `${rank} ${lastName} reporting for duty!`;
+  }
+
+console.log(reportingForDuty('Private', 'Neal')); // Prints Private Neal reporting for duty!
+
+/* 7 April 2021
+Though an object’s mass remains consistent throughout the universe, weight is determined by 
+the force of gravity on an object. Since different planets have different gravity, the same 
+object would weigh different amounts on each of those planets! Write a function, 
+calculateWeight(). 
+It should:
+    have two parameters: earthWeight and planet
+    expect earthWeight to be a number
+    expect planet to be a string
+    return a number representing what that Earth-weight would equate to on the planet passed in.
+Handle the following cases:
+    'Mercury' weight = earthWeight * 0.378
+    'Venus' weight = earthWeight * 0.907
+    'Mars' weight = earthWeight * 0.377
+    'Jupiter' weight = earthWeight * 2.36
+    'Saturn' weight = earthWeight * 0.916
+    For all other inputs, return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, 
+    Jupiter, or Saturn.' */
+
+const calculateWeight = (earthWeight, planet) => { 
+    switch (planet) {
+        case 'Mercury':
+        return earthWeight * .378;
+        break;
+        case 'Venus':
+        return earthWeight * .907;
+        break;
+        case 'Mars':
+        return earthWeight * .377;
+        break;
+        case 'Jupiter':
+        return earthWeight * 2.36;
+        break;
+        case 'Saturn':
+        return earthWeight * .916;
+        break;
+        default:
+        return 'Invalid Planet Entry. Try: Mecury, Venus, Mars, Jupiter, or Saturn.';
+        break;
+    }
+    }
+
+console.log(calculateWeight(100, 'Jupiter')) // Prints 236
